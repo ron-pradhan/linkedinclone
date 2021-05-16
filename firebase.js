95605c948d63df561ee7f568bbaa36c39e077745
Ronit Pradhan <iamronit94@gmail.com>
2021-05-16 11:38:14 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBkPtAG06o503I21ElbjpD_x6mwkPgYiAg",
+    authDomain: "linkedin-clone-2d5cb.firebaseapp.com",
+    projectId: "linkedin-clone-2d5cb",
+    storageBucket: "linkedin-clone-2d5cb.appspot.com",
+    messagingSenderId: "270446921544",
+    appId: "1:270446921544:web:7b17ca041d4cf84a5d957d"
+  };
